81e4f789386c7def9e1a3cd6d82dc2f37462cc8c
pranav shinde <pranavdshinde775@gmail.com>
2025-03-07 19:15:19 +0530

    first commit

---
+++ test1.jl
@@ -0,0 +1,15 @@
+println("hello world")
+
+
+function print_numbers()
+    for i in range(1, 10)
+        println(i) 
+        
+    end
+    return 1:10
+    
+end
+print_numbers()
+
+import Pkg
+Pkg.add("Pluto")
